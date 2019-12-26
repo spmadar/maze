8e7d895764918a1b77e7a123a23508ca7ac8b659
Sophia Madar <spmadar@gmail.com>
2019-12-26 16:28:28 -0500

    initial commit

---
+++ functions.js
@@ -0,0 +1,24 @@
+let mazeBoard = document.getElementById("mazeBoard");
+
+let posX = 0;
+let posY = 9;
+
+let winPos = false;
+
+let board = [
+    "WWWWWWWWWWWWWWWWWWWWW",
+    "W   W     W     W W W",
+    "W W W WWW WWWWW W W W",
+    "W W W   W     W W   W",
+    "W WWWWWWW W WWW W W W",
+    "W         W     W W W",
+    "W WWW WWWWW WWWWW W W",
+    "W W   W   W W     W W",
+    "W WWWWW W W W WWW W F",
+    "S     W W W W W W WWW",
+    "WWWWW W W W W W W W W",
+    "W     W W W   W W W W",
+    "W WWWWWWW WWWWW W W W",
+    "W       W       W   W",
+    "WWWWWWWWWWWWWWWWWWWWW"
+];
